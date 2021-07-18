348c8afbe10d7d2ea932f44559d8b6cd31b309b8
Ryan Thomas <ryanwhthomas@gmail.com>
2021-07-18 02:14:51 -0400

    Input around grid; Dynamic dimensions.
    Input boxes appear around the grid. They currently do nothing.
    
    Enter button was removed. Grid now changes size dynamically. Grid will now check that dimension is a valid integer in range between the hardcoded minimum and maximum.
    
    Solve and reset buttons added. They're currently functionless, except for a TODO alert.
    
    Altered the icon as a test.

--- src/index.js
+++ src/index.js
@@ -3,99 +3,185 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+// TODO: make personal icon
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { dim: 6, grid: null };
+
+    this.range = [1, 9];
+    // TODO: pass grid in a valid way?
+    this.state = { dim: 6, inputs: this.resetInputs(), grid: null };
   }
 
   setNewDim(newDim) {
     this.setState({ dim: newDim })
   }
 
+  resetInputs() {
+    const sides = ["left", "right", "top", "bottom"];
+    let map = new Map();
+
+    sides.forEach(x => map.set(x, null));
+    return map;
+  }
+
   // TODO write this method
   resetGrid() {
+    // reset values in middle of grid
+    // reset values in input numbers
+    // should call resetInputs
     alert("Write this method!");
   }
 
+  // TODO write this method
+  solveGrid() {
+    alert("Write this method!");
+  }
+
+  // TODO write this method
+  updateInput(side, index, value) {
+    alert("updateValues called " + side + " " + index + " " + value);
+    const inputs = this.state.inputs;
+    let arr = inputs.get(side);
+
+    if (arr === null) { arr = (new Array(this.state.dim)).fill(null); }
+
+    arr[index] = value;
+    inputs.set(side, arr);
+
+    this.setState({
+      inputs: inputs
+    })
+  }
+
   render() {
-    const range = [1, 9];
+    const min = this.range[0];
+    const max = this.range[1];
 
     return (
       <div>
         <h1>Skyscraper Solver</h1>
-        <Input startDim={this.state.dim} min={range[0]} max={range[1]} onSubmit={(x) => this.setNewDim(x)} />
-        <Grid dim={this.state.dim} />
+        <DimInput startDim={this.state.dim} min={min} max={max} onEnter={(x) => this.setNewDim(x)} />
+        <Grid dim={this.state.dim} min={min} max={max} updateInput={(x, y, z) => this.updateInput(x, y, z)} />
+        <button onClick={this.solveGrid}>
+          Solve
+        </button>
         <button onClick={this.resetGrid}>
           Reset Grid
         </button>
+
       </div>
     );
   }
 }
 
-class Input extends React.Component {
+class DimInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: this.props.startDim };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleEnter = this.handleEnter.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+    // this.setState({ value: event.target.value });
 
-  handleSubmit(event) {
-    event.preventDefault();
+    let parsed = parseInt(event.target.value);
 
-    const parsed = parseInt(this.state.value);
-    if (isNaN(parsed)) {
-      alert("Enter valid number.")
-    } else {
-      this.props.onSubmit(parsed);
+    console.log("Entered value:\t" + parsed)
+
+    // ignore non-numerical values
+    if (!isNaN(parsed)) {
+      // TODO: should alerts tell the user when the values exceed boundaries?
+      // ensure any numerical values are betwee min and max
+      parsed = Math.min(parsed, this.props.max);
+      parsed = Math.max(parsed, this.props.min)
+
+      this.props.onEnter(parsed);
     }
+
+    this.setState({ value: parsed });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <div>
         <label>
           Enter dimensions (between {this.props.min} and {this.props.max}):
         </label>
         <input type="number" value={this.state.value} min={this.props.min} max={this.props.max} onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
+      </div>
     );
   }
 }
 
 class Grid extends React.Component {
+  // TODO: set input's value to what the user inputs
+  getSideInput(side, index) {
+    return (<SideInput max={this.props.dim}
+      // value = {this.props.getInputs()}
+      updateInput={(x) => this.props.updateInput(side, index, x)} />);
+  }
+
+  inputRow(side) {
+    const temp = this.getIndicesArray().map(c => this.getSideInput(side, c));
+    return (<tr><th></th>{temp}<th></th></tr>);
+  };
+
+  getIndicesArray() {
+    return ([...Array(this.props.dim).keys()]);
+  }
+
   render() {
     const dim = this.props.dim;
 
-    if (dim > 0) {
-      // alert("New dim is " + dim + ". Type is " + typeof (dim));
-      const indices = [...Array(dim).keys()];
+    const indices = this.getIndicesArray();
 
-      const table = indices.map(c => {
-        let row = indices.map(r => <th>[{c + 1}, {r + 1}] </th>);
-        return <tr>{row}</tr>;
-      }
+    const table = indices.map(r => {
+      let row = indices.map(c => <th>[{c + 1}, {r + 1}] </th>);
 
-      );
-
-      return (
-        <div>
-          <h1> Grid {dim} </h1>
-          <table>
-            {table}
-          </table>
-        </div>);
-    } else {
-      return (<h1> Grid Goes Here </h1>)
+      return (<tr>
+        {this.getSideInput("left", r)} {row} {this.getSideInput("right", r)}
+      </tr>)
     }
+    );
+
+    return (
+      <div>
+        <h1> Grid {dim} </h1>
+        <table>
+          {this.inputRow("top")}
+          {table}
+          {this.inputRow("bottom")}
+        </table>
+      </div>);
+
+  }
+}
+
+class SideInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.updateInput(event.target.value);
+  }
+
+  render() {
+    return (
+      <th>
+        <input type="number"
+          // TODO: should value be passed down from App?
+          value={null} min={1} max={this.props.max}
+          // onChange={() => this.props.updateValues(side, index, this.value)}
+          onChange={this.handleChange}
+        />
+      </th>);
   }
 }
 
